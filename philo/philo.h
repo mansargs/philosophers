cbc6a9f24a39e3a6229fcc039c11618cdcf459aa
Manvel Sargsyan <mansargs@c2r3s6.42yerevan.am>
2025-05-27 21:29:21 +0400

    first commit

---
+++ philo/philo.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mansargs <mansargs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 21:13:57 by mansargs          #+#    #+#             */
+/*   Updated: 2025/05/27 21:28:23 by mansargs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <time.h>
+# include <errno.h>
+
+# define TAKE_FORK "has taken a work"
+# define EATING "is eating"
+# define SLEEPING "is sleeping"
+# define THINKING "is thinking"
+# define DIED "died"
+# define INVALID_ARGC "You need to run the program like this ./program_name number_of_philosopher time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]"
+
+
+#endif
