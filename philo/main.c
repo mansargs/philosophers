2bed7638da019486e97b449266689b11791e077d
Manvel Sargsyan <mansargs@c2r3s6.42yerevan.am>
2025-05-27 21:54:50 +0400

    valid arguments check

--- philo/main.c
+++ philo/main.c
@@ -6,16 +6,46 @@
 /*   By: mansargs <mansargs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 21:20:26 by mansargs          #+#    #+#             */
-/*   Updated: 2025/05/27 21:22:54 by mansargs         ###   ########.fr       */
+/*   Updated: 2025/05/27 21:49:18 by mansargs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char *argv[])
+static bool	valid_number(const char *str)
 {
-	if (argc < 5 || argc > 6)
+	int	i;
+
+	i = -1;
+	if (str[0] == '+')
+		++i;
+	while (str[++i])
 	{
-		
+		if (str[i] < '0' || str[i] > '9')
+			return (false);
 	}
+	return (true);
+}
+
+static bool	valid_arguments(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		if (!valid_number(argv[i]))
+			return (false);
+	}
+	return (true);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6 || !valid_arguments(argc, argv))
+	{
+		printf(INVALID_ARGC);
+		return (EINVAL);
+	}
+	return (EXIT_SUCCESS);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: mansargs <mansargs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 21:13:57 by mansargs          #+#    #+#             */
-/*   Updated: 2025/05/27 21:28:23 by mansargs         ###   ########.fr       */
+/*   Updated: 2025/05/27 21:54:08 by mansargs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@
 # include <pthread.h>
 # include <time.h>
 # include <errno.h>
+# include <stdbool.h>
 
 # define TAKE_FORK "has taken a work"
 # define EATING "is eating"
 # define SLEEPING "is sleeping"
 # define THINKING "is thinking"
 # define DIED "died"
-# define INVALID_ARGC "You need to run the program like this ./program_name number_of_philosopher time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]"
+# define INVALID_ARGC "\033[31mYou need to run the program like this\n./philo number_of_philosopher time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\033[0m\n"
 
 
 #endif
